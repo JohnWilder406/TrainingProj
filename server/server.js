1f74c0a8e9f62fcc361ba17a314f6b0ad27691d9
John Wilder <john.wilder@gmail.com>
2021-07-16 13:42:16 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = process.env.MY_PORT;
+
+// require('./config/mongoose.config')
+
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// require('./routes/customer.routes')(app)
+// require('./routes/user.routes')(app)
+// require('./routes/product.routes')(app)
+
+app.listen(port, ()=> console.log(`Listening on port: ${port}`));
