b225157e27ed7dddfd43c8b34e76b817f00c7ae3
John Wilder <john.wilder@gmail.com>
2021-07-19 15:56:57 -0700

    Admin Main, Form, and Add

--- client/src/views/addTraining.js
+++ client/src/views/addTraining.js
@@ -2,10 +2,39 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {navigate, Link} from '@reach/router';
 import { Container, Card, Form, Row, Col } from  'react-bootstrap';
+import ModularForm from '../components/form';
 
 const AddTraining = (props) => {
+    const [errors, setErrors] = useState({})
+    const [plan, setPlan] = useState({
+        name: "",
+        difficulty: "",
+        duration: "", 
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/plans/add_plan', plan)
+            .then((res) => {
+                console.log(res.data)
+                if(res.data.errors) {
+                    setErrors(res.data.errors)
+                } else {
+                    navigate('/admin/main')
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+
+
     return (
-        <h1>Add Training Plan Page</h1>
+        <Container>
+            <h1>Add Training Plan Page</h1>
+            <ModularForm object={plan} setObject={setPlan} errors={errors} handleSubmit={handleSubmit} submitLabel={"Add Plan"} />
+        </Container>
     )
 }
 
--- server/routes/training.routes.js
+++ server/routes/training.routes.js
@@ -3,7 +3,8 @@ const TrainingController = require('../controllers/training.controller');
 module.exports = function(app){
     app.get('/api/plans', TrainingController.getAllTraining);
     app.get('/api/plans/:id', TrainingController.getTraining);
-    app.post('/api/plans', TrainingController.createTraining);
+    app.post('/api/plans/add_plan', TrainingController.createTraining);
     app.put('/api/plans/:id', TrainingController.updateTraining);
     app.delete('/api/plans/:id',TrainingController.deleteTraining);
+    app.put('/api/plans/:id/add_workout', TrainingController.createWorkout)
 }
