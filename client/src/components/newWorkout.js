8e210b0aa641328ac27f12e20a68c07791ad7e34
April Hughes <ahughes0389@gmail.com>
2021-07-23 21:46:34 -0700

    Table is functioning!

--- client/src/components/newWorkout.js
+++ client/src/components/newWorkout.js
@@ -5,23 +5,33 @@ import { Container, Card, Form, Row, Col, Button, Table } from  'react-bootstrap
 import Navbar from './Navbar';
 
 const NewWorkout = (props) => {
-    const [plan, setPlan] = useState({});
-    const [workouts, setWorkouts] = useState([]);
-
+    const [user, setUser] = useState([]);
+    const [training, setTraining] = useState([]);
+    const [workout, setWorkout] = useState([]);
+    const {id} = props;
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/plans')
             .then((res) => {
                 console.log(res);
-                setPlan(res.data);
-                setWorkouts(res.data.workouts);
-                console.log("workouts " + workouts.data);
+                setTraining(res.data);
             })
             .catch((err) => {
                 console.log(err);
             })
     }, []);
 
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/user/get/' + id)
+            .then((res) => {
+                console.log(res);
+                setUser(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, []);
+    
     return (
         <Container>
             <h1>New Workout (user)</h1>
@@ -58,29 +68,29 @@ const NewWorkout = (props) => {
                             <th>Add</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {/* {
-                            workouts.map((plan, index) => (
-                                <tr key={index}>
-                                    <td>{plan.name}</td>
-                                    <td>{plan.duration}</td>
-                                    <td>{plan.intensity}</td>
-                                    <td>{plan.difficulty}</td>
-                                    <td>{plan.frequency}</td>
-                                    <td>Add</td>
-                                </tr>
-                            ))
-                        } */}
-                        <tr>
-                            <td>workout</td>
-                            <td>duration</td>
-                            <td>intensity</td>
-                            <td>difficulty</td>
-                            <td>freq/week</td>
-                            <td>add</td>
-                        </tr>
-                    </tbody>
-                    </Table>
+                    {
+                        training.map((plan, index) => {
+                            return (
+                                <tbody key={index}>
+                                    {
+                                        plan.workouts.map((workouts, i) => {
+                                            return (
+                                                <tr key={i}>
+                                                    <td>{workouts.name}</td>
+                                                    <td>{workouts.duration}</td>
+                                                    <td>{workouts.intensity}</td>
+                                                    <td>{workouts.difficulty}</td>
+                                                    <td>{workouts.frequency}</td>
+                                                    <td>Add</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            )
+                        })
+                    }
+                </Table>
             </div>
         </Container>
     );
